9e3e81d35088dc1ed49a12344442a03e55c7bb65
Riaño John <jhon091199@gmail.com>
2024-09-29 18:13:19 -0500

    fixes commmands netsuite

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('uploader-netsuite.helloWorld', async () => {
+	const disposable = vscode.commands.registerCommand('uploader-netsuite.Netsuite:Push File', async () => {
 		const activeEditor = vscode.window.activeTextEditor;
 		if (activeEditor) {
 			const filePath = activeEditor.document.uri.fsPath;
