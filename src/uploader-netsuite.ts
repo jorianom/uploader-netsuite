66d822c99b4de9315d48f6b0bddba9e38bda9acd
Riaño John <jhon091199@gmail.com>
2024-09-29 17:59:27 -0500

    send to netsuite restlet 1.0

--- src/uploader-netsuite.ts
+++ src/uploader-netsuite.ts
@@ -3,7 +3,6 @@ import { readFile } from 'fs/promises';
 import axios from 'axios'
 import crypto from 'crypto';
 import oauth from 'oauth-1.0a';
-import FormData from 'form-data';
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
@@ -18,53 +17,20 @@ const fielUpload = async (filePath: string) => {
     }
     sendFile(filePath);
 }
-async function sendFile(filePath: string) {
-    const config = vscode.workspace.getConfiguration('uploaderNetSuite');
-
-    const url = config.get<string>('urlScript') ?? '';
-    const consumerKey = config.get<string>('consumerKey') ?? '';
-    const consumerSecret = config.get<string>('consumerSecret') ?? '';
-    const accessToken = config.get<string>('accessToken') ?? '';
-    const tokenSecret = config.get<string>('tokenSecret') ?? '';
-    const realm = config.get<string>('realm') ?? '';
-
-    // Crear un objeto FormData
-    const form = new FormData();
-    form.append('file', fs.createReadStream(filePath));
+const getFileData = (filePath: string) => {
     let fileContents = fs.readFileSync(filePath, 'base64');
     let filename = path.basename(filePath);
-    const data = {
+    return {
         fileContents,
         filename
     }
-    console.log('Form Data:', form);
-    let oauth1 = new oauth({
-        consumer: {
-            key: consumerKey,
-            secret: consumerSecret,
-        },
-        signature_method: 'HMAC-SHA256',
-        hash_function(base_string, key) {
-
-            console.log('Base String:', base_string);
-            console.log('Key:', key);
-            return crypto.createHmac('sha256', key).update(base_string).digest('base64');
-        },
-    });
-    let token = {
-        key: accessToken,
-        secret: tokenSecret,
-    };
-    const request_data = {
-        url: url,
-        method: 'POST',
-    };
-
-    const authHeaders = oauth1.toHeader(oauth1.authorize(request_data, token));
-    authHeaders['Authorization'] += `, realm="${realm}"`; // Añade el realm
-    console.log('Headers:', authHeaders['Authorization']);
+}
+async function sendFile(filePath: string) {
+    const config = vscode.workspace.getConfiguration('uploaderNetSuite');
+    const { url, consumerKey, consumerSecret, accessToken, tokenSecret, realm } = getVariablesAuth(config);
+    let data = getFileData(filePath);
+    let authHeaders = getAuthorization(consumerKey, consumerSecret, accessToken, tokenSecret, realm, url);
     try {
-        // Enviar la petición POST
         const response = await axios.post(url, data, {
             headers: {
                 ...authHeaders,
@@ -83,22 +49,37 @@ async function sendFile(filePath: string) {
     }
 }
 
-const getAuthorization = (consumerKey: string, consumerSecret: string, accessToken: string, tokenSecret: string, realm: string) => {
-    const oauthTimestamp = Math.floor(Date.now() / 1000);
-    const oauthNonce = crypto.randomBytes(16).toString('hex');
-    const oauthSignatureMethod = 'HMAC-SHA256';
-    const oauthVersion = '1.0';
-    // Generar `oauth_signature`
-    const baseString = 'tu_base_string'; // Reemplaza con tu base string (URL, parámetros, etc.)
-    const signingKey = 'consumer_secret&token_secret'; // Reemplaza con tu consumer secret y token secret
-
-    const oauthSignature = crypto.createHmac('sha256', signingKey)
-        .update(baseString)
-        .digest('base64');
-
-    // Crear el header de autorización OAuth
-    const authHeader = `OAuth realm="${realm}",oauth_consumer_key="${consumerKey}",oauth_token="${accessToken}",oauth_signature_method="${oauthSignatureMethod}",oauth_timestamp="${oauthTimestamp}",oauth_nonce="${oauthNonce}",oauth_version="${oauthVersion}",oauth_signature="${encodeURIComponent(oauthSignature)}"`;
-    return authHeader;
+const getVariablesAuth = (config: vscode.WorkspaceConfiguration) => {
+    const url = config.get<string>('urlScript') ?? '';
+    const consumerKey = config.get<string>('consumerKey') ?? '';
+    const consumerSecret = config.get<string>('consumerSecret') ?? '';
+    const accessToken = config.get<string>('accessToken') ?? '';
+    const tokenSecret = config.get<string>('tokenSecret') ?? '';
+    const realm = config.get<string>('realm') ?? '';
+    return { url, consumerKey, consumerSecret, accessToken, tokenSecret, realm };
+}
+const getAuthorization = (consumerKey: string, consumerSecret: string, accessToken: string, tokenSecret: string, realm: string, url: string) => {
+    let oauth1 = new oauth({
+        consumer: {
+            key: consumerKey,
+            secret: consumerSecret,
+        },
+        signature_method: 'HMAC-SHA256',
+        hash_function(base_string, key) {
+            return crypto.createHmac('sha256', key).update(base_string).digest('base64');
+        },
+    });
+    let token = {
+        key: accessToken,
+        secret: tokenSecret,
+    };
+    const request_data = {
+        url: url,
+        method: 'POST',
+    };
+    const authHeaders = oauth1.toHeader(oauth1.authorize(request_data, token));
+    authHeaders['Authorization'] += `, realm="${realm}"`; // Añade el realm
+    return authHeaders;
 }
 
 const validateFile = (data: string, filePath: string) => {
